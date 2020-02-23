bc09251b2df019d36006b114879ec7c29605f313
“FORZENLENO <1113650154@qq.com>
2020-02-23 23:51:53 +0800

    stu

--- src/views/personnel/student-management.vue
+++ src/views/personnel/student-management.vue
@@ -2,15 +2,36 @@
   <div v-loading="loading" class="student-management">
     <el-row :gutter="40" type="flex" justify="space-between">
       <el-col :span="4">
-        <el-button type="success" @click="showForm()">添加班级</el-button>
+        <el-button type="success" @click="showFormB()">添加班级</el-button>
+        <div v-if='addClazFlag.visible'>
+          <el-input  v-model="addClazFlag.addClazInfo" placeholder="请输入班级名称"></el-input>
+          <el-button type="success" @click="addClaz(addClazFlag.addClazInfo)">确定</el-button>
+        </div>
         <el-tree 
           class="filter-tree" 
           :data="list" 
           :props="props" 
+          node-key="id"
           default-expand-all
           accordion 
-          :filter-node-method="filterNode" 
           ref="tree">
+          <span class="custom-tree-node" slot-scope="{ node, data }">
+            <span>{{ node.label }}</span>
+            <span>
+              <el-button
+                type="text"
+                size="mini"
+                @click="() => append(data)">
+                Append
+              </el-button>
+              <el-button
+                type="text"
+                size="mini"
+                @click="() => removeClass(data)">
+                删除
+              </el-button>
+            </span>
+          </span>
         </el-tree>
       </el-col>
       <el-col :span="20" style="box-shadow:0 2px 12px 0 rgba(0, 0, 0, .12);margin-right：100px">
@@ -47,6 +68,17 @@
         </lkt-table>
       </el-col>
     </el-row>
+    <!-- <kit-dialog-simlpe 
+      :modal="addClaz"
+      :confirm="clazUpdate"
+      width="700px">
+      <div slot="title">增加班级</div>
+      <el-form v-if="addClaz.visible" ref="clazForm" :model="addClaz.clazInfo" label-width="120px" label-position="left" style="width: 580px;margin: 0 auto">
+          <el-form-item label="班级名称" prop="clazName" :rules="{ required: true, message: '请输入班级名称'}">
+              <el-input v-model="addClaz.clazInfo.clazName"></el-input>
+          </el-form-item>
+        </el-form>
+    </kit-dialog-simlpe> -->
     <kit-dialog-simple
       :modal="modal"
       :confirm="update"
@@ -57,19 +89,15 @@
               <el-input v-model="modal.studentInfo.name"></el-input>
           </el-form-item>
           <el-form-item label="班级" prop="extend.clasz" :rules="{ required: true, message: '请选择班级'}">
-              <!-- <el-input v-model="modal.studentInfo.extend.clasz"></el-input> -->
-              <!-- <el-select v-model="modal.studentInfo.extend.clasz" @change="armasd()">
-                <el-option v-for="clas in classList" :key="clas.id" :label="clas.name" :value="clas.name" />
-              </el-select> -->
-              <el-cascader
-                v-model="modal.studentInfo.extend.clasz"
-                :options="blist"></el-cascader>
-          </el-form-item>
-          <!-- <el-form-item label="教学分组" prop="extend.claszGroup" :rules="{ required: true, message: '请选择分组'}">
-              <el-select v-model="modal.studentInfo.extend.claszGroup">
-                <el-option v-for="grop in groupList" :key="grop.id" :label="grop.name" :value="grop.name" />
+              <el-select v-model="modal.studentInfo.extend.clasz" @change="armasd(modal.studentInfo.extend.clasz)">
+                <el-option v-for="clas in classList" :key="clas.id" :label="clas.name" :value="clas.id" />
               </el-select>
-          </el-form-item> -->
+          </el-form-item>
+          <el-form-item label="教学分组" prop="extend.claszGroup" :rules="{ required: true, message: '请选择分组'}">
+              <el-select v-model="modal.studentInfo.extend.claszGroup">
+                <el-option v-for="grop in groupList" :key="grop.id" :label="grop.name" :value="grop.id" />
+              </el-select>
+          </el-form-item>
           <el-form-item label="登录用户名" prop="username" :rules="{ required: true, message: '请输入登录用户名'}">
               <el-input v-model="modal.studentInfo.username"></el-input>
           </el-form-item>
@@ -97,18 +125,16 @@ import { useSearch, useConfirm, useLoading } from 'web-toolkit/src/service';
 import {postService, storeUserInfo} from 'web-toolkit/src/case-main';
 import { Message } from 'element-ui';
 import {isUndefined, deepClone} from 'web-toolkit/src/utils';
-import {StudentList, UserAdd, RoleList, UserDel, UserUpdate, ClassList4Select, ClassList} from '@/dao/userDao';
+import {StudentList, UserAdd,RoleList,UserDel,UserUpdate,ClassList4Select,ClassList,ClassUpdate,ClassDel} from '@/dao/userDao';
 export default {
   setup() {
-    function armasd() {
-      console.log('!');
-    }
     const loading = ref(false);
     const tree = ref<ElTree<any, any>|null>(null);
     const filterText = ref<string|null>(null);
     const list = ref<any>();
     const studentUserList = ref<any>();
     const classList = ref<any>();
+    const ctogList = ref<any>();
     const groupList = ref<any>();
     const blist = ref<any>();
     const props = ref({
@@ -122,6 +148,13 @@ export default {
         });
         await queryStudentList();
     };
+    const removeClass = async (row:any)=>{
+        await ClassDel({
+          id:row.id
+        })
+        console.log(row.id)
+        await queryClassList();
+    }
     watch(filterText, () => {
       if (tree.value) { (tree.value as ElTree<any, any>).filter(filterText.value); }
     });
@@ -129,10 +162,15 @@ export default {
       includeProps: ['username', 'name', 'phone' , 'address', 'extend.clasz', 'extend.claszGroup'],
     });
     const form = ref<ElForm|null>(null);
+    // const clazForm = ref<ElForm|null>(null);
     const modal = ref<any>({
       visible: false,
       studentInfo: null,
     });
+    // const addClaz = ref<any>({
+    //   visible: false,
+    //   clazInfo: null,
+    // });
     async function toggleStatus(row: any) {
       const off = row.off;
       await UserDel({
@@ -151,17 +189,48 @@ export default {
         callback();
       }
     }
-    // const showForm = async (data?: any) => {
-    //   if (form.value) { (form.value as ElForm).clearValidate(); }
-    //   if (data) {
-    //     data = deepClone(data);
-
-    //   } else {
-    //     data = initForm();
+    const addClazFlag  =  ref<any>({
+      visible: false,
+      addClazInfo: '',
+    });
+    function showFormB(row:any){
+    //   addClaz.value.clazInfo = row ? deepClone(row) :initClazForm();
+    //   addClaz.value.visible = true;
+    //   if (clazForm.value) { (clazForm.value as ElForm).clearValidate(); }
+    // }
+    // async function clazUpdate(){
+    //   const valid = await (clazForm.value as ElForm).validate();
+    //   const result = {
+    //     clazName:addClaz.value.clazInfo.clazName,
     //   }
-    //   modal.value.studentInfo = data;
-    //   modal.value.visible = true;
-    // };
+    //   await ClassUpdate(result);
+        addClazFlag.value.visible = !addClazFlag.value.visible;
+    }
+    async function addClaz(row: any){
+      if(addClazFlag.value.addClazInfo!=''){
+        const result={
+          name:addClazFlag.value.addClazInfo
+        }
+        // const i=await ClassList();
+        // const iFlag =  ref<any>({
+        //   visible: true
+        // });
+        // for(let j=0;j<i.length;j++){
+        //   if(result.name=i[j].name){
+        //     return iFlag.value.visible=false
+        //   }
+        //   return iFlag.value.visible
+        // }
+        // console.log(i)
+        // console.log(iFlag)
+        await ClassUpdate(result)
+        addClazFlag.value.visible = !addClazFlag.value.visible;
+        await queryClassList();
+      }
+      else{
+        addClazFlag.value.visible = !addClazFlag.value.visible;
+      }
+    }
     function showForm(row: any) {
       if (row) {
         row.pwd = '';
@@ -174,9 +243,6 @@ export default {
     async function update() {
       const valid = await (form.value as ElForm).validate();
       if (!valid) { return ; }
-      // watch(modal.studentInfo.extend.clasz,()=>{
-      //   console.log("变了！")
-      // })
       const result = {
         id: modal.value.studentInfo.id,
         // address: addModal.value.teacherInfo.address,
@@ -207,52 +273,45 @@ export default {
         const firstList = await StudentList();
         studentUserList.value  = firstList;
     };
-    const filterNode = async (value: any, data: any) => {
-      if (!value) {return true; }
-      return data.label.indexOf(value) !== -1;
-    };
-    onMounted(useLoading(loading, async () => {
-      classList.value = await ClassList4Select();
-      // groupList.value = classList.value[0].groups
-      // console.log(classList)
-      // console.log(classList.value[0].groups)
-      // console.log(groupList)
-      await queryStudentList();
-      const aList = await ClassList();
-      list.value = [];
-      blist.value = [];
-      list.value.children = [];
-      blist.value.children = [];
-      for (let i = 0; i < aList.length; i++) {
-        list.value[i] = {id: aList[i].id, name: aList[i].name, children: [...aList[i].groups]};
-        blist.value[i] = {value: aList[i].name, label: aList[i].name, children: [...aList[i].groups]};
+    async function queryClassList(){
+      const aList=await ClassList();
+      list.value=[];
+      blist.value=[];
+      list.value.children=[];
+      blist.value.children=[];
+      for(let i=0;i<aList.length;i++){
+        list.value[i]={id:aList[i].id,name:aList[i].name,children:[...aList[i].groups]}
+        blist.value[i]={value:aList[i].name,label:aList[i].name,children:[...aList[i].groups]}
       }
-      // console.log(list.value[0].children)
-      // for(let i=0;i<list.value[i].length;i++){
-      //   for(let j=0;j<list.value[i].children.length;j++){
-      //     blist.value[i].children=[{value:list.value[i].children[j].name,label:list.value[i].children[j].name}]
-      //     console.log(j)
-      //   }
-      // }
-      // blist.value[0].children[0]={value:list.value[0].children[0].name,label:list.value[0].children[0].name}
-      // blist.value[0].children[1]={value:list.value[0].children[1].name,label:list.value[0].children[1].name}
-      // blist.value[0].children=[
-      //   {value:list.value[0].children[0].name,label:list.value[0].children[0].name},
-      //   {value:list.value[0].children[1].name,label:list.value[0].children[1].name}
-      // ]
-      console.log(list);
-      console.log(blist);
-      // console.log(list)
+      console.log(aList)
+      console.log(list)
+    }
+    function armasd(row:any){
+      modal.value.studentInfo.extend.claszGroup='';
+      const midList = ref<any>();
+      midList.value=[]
+      for(let i=0;i<classList.value.length;i++){
+        midList.value[i]=classList.value[i].name
+      }
+      groupList.value = classList.value[row-1].groups
+      return groupList
+    }
+    onMounted(useLoading(loading, async () => {
+      classList.value = await ClassList();
+      await queryStudentList();
+      await queryClassList();
     }));
     return{
-      loading, filterText, list, tree, props, filterNode, studentUserList, filtered, keywords, blist,
-      storeUserInfo,
+      loading, filterText, list, tree, props, studentUserList,filtered,keywords,blist,addClazFlag,
+      storeUserInfo,removeClass:useConfirm('确认删除？', useLoading(loading, removeClass)),
+      armasd,
       remove: useConfirm('确认删除？', useLoading(loading, remove)),
       toggleStatus: useLoading(loading, toggleStatus),
-      queryStudentList,
-      modal, form, showForm,
+      queryStudentList,queryClassList,
+      modal, form, showForm,addClaz,
+      showFormB,
       update: useLoading(loading, update),
-      validator, classList, groupList,
+      validator,classList,groupList,ctogList,
     };
     return{
 
@@ -269,7 +328,19 @@ function initForm() {
     extend: {clasz: '', claszGroup: ''},
   };
 }
+// function initClazForm(){
+//   return {
+//     clazName:''
+//   }
+// }
 </script>
 <style scoped lang="scss">
-
+  .custom-tree-node {
+    flex: 1;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    font-size: 14px;
+    padding-right: 8px;
+  }
 </style>
