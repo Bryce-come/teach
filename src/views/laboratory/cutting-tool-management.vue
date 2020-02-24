bb5d7593056ab03a7f77d1e31285272df79de1c6
cheny <731820536@qq.com>
2020-02-24 17:30:26 +0800

    刀具错误

--- src/views/laboratory/cutting-tool-management.vue
+++ src/views/laboratory/cutting-tool-management.vue
@@ -3,18 +3,9 @@
     <div style="margin-bottom:10px">
       <el-button type="primary" @click="cutterForm()">增加刀具信息</el-button>
     </div>  
-    <el-form :inline="true" style="display:flex;justify-content:flex-start;flex-wrap:wrap">
-      <el-form-item label="刀具名称:" label-width="80px">
-        <lkt-select :list="cutterNameList" value-key="name" v-model="cutterName" multiple :clearable="false" placeholder="请选择刀具名称"/>
-      </el-form-item>
-      <el-form-item label="刀具型号:" label-width="80px">
-        <lkt-select :list="cutterNoList" value-key="no" v-model="cutterNo" multiple :clearable="false" placeholder="请选择刀具型号"/>
-      </el-form-item>
-      <el-form-item>
-        <el-button type="primary">查询</el-button>
-        <el-button style="margin-left:10px">重置</el-button>
-      </el-form-item>
-    </el-form>
+    <div class="flex end" style="margin-bottom: 5px;margin-top: -46px">
+      <el-input slot="search" class="search-bar" v-model="keywords" placeholder="按关键字搜索" clearable/>
+    </div>
     <lkt-table
       :data="cutterNameList"
       style="width:100%">
@@ -32,17 +23,17 @@
     <kit-dialog-simple
       :modal="addModal"
       :confirm="cutterInfoUpdate"
-      width="700px">
+      width="500px">
         <div slot="title">刀具信息登记</div>
-        <el-form v-if="addModal.cutterInfo" ref="form" :model="addModal.cutterInfo" label-width="160px" label-position="left" style="width: 580px;margin: 0 auto">
+        <el-form v-if="addModal.cutterInfo" ref="form" :model="addModal.cutterInfo" label-width="160px" label-position="left" style="width: 377px;margin: 0 auto">
           <el-form-item label="刀具名称：" prop="name" :rules="{ required: true, message: '请输入刀具名称'}">
             <el-input v-model="addModal.cutterInfo.name"></el-input>
           </el-form-item>
           <el-form-item label="刀具型号：" prop="no" :rules="{ required: true, message: '请选择刀具型号'}">
-              <lkt-select v-model="addModal.cutterInfo.no"></lkt-select>
+              <el-input v-model="addModal.cutterInfo.no" ></el-input>
           </el-form-item>
           <el-form-item label="适配设备型号(多选)：" prop="fitDeviceType" :rules="{ required: true, message: '请选择适配设备型号'}">
-              <lkt-select v-model="addModal.cutterInfo.fitDeviceType" multiple></lkt-select>
+              <lkt-select :list="deviceTypeList" value-key="name" option-value-key="id" v-model="addModal.cutterInfo.fitDeviceType.id" multiple></lkt-select>
           </el-form-item>
         </el-form>
     </kit-dialog-simple>
@@ -99,19 +90,22 @@
 <script lang="ts">
 import { ref, Ref, onMounted } from '@vue/composition-api';
 import { Message } from 'element-ui';
-import { useConfirm, useLoading } from 'web-toolkit/src/service';
+import { useConfirm, useLoading, useSearch } from 'web-toolkit/src/service';
 import {ElForm} from 'element-ui/types/form';
 import {isUndefined, deepClone} from 'web-toolkit/src/utils';
+import {ComponentStoreAdd, ComponentStoreUpdate, ComponentStoreDel, ComponentStoreList, ComponentStoreRecordAdd, ComponentStoreRecordUpdate, ComponentStoreRecordDel } from '@/dao/componentStoreDao';
+import { DeviceTypeList,} from '@/dao/deviceDao';
 export default {
   setup() {
     const loading = ref(false);
-    const cutterNameList = ref<any>();
-    const cutterName = ref<any>();
-    const cutterNoList = ref<any>();
-    const cutterNo = ref<any>();
+    const cutterList = ref<any>();
+    const deviceTypeList = ref<any>();
     const deviceComponentStore = ref<any>();
     const deviceComponentStoreRecord = ref<any>();
     const filterText = ref<string|null>(null);
+    const [keywords, cutterNameList] = useSearch(cutterList, {
+      includeProps: ['no', 'fitDeviceType', 'name', 'quantity' ,'extend.discardNum'],
+    });
     const remove = async (row: any) => {
         Message.success('删除成功');
     };
@@ -119,14 +113,16 @@ export default {
     const addModal = ref<any>({
       visible: false,
       cutterInfo: null,
+      type: 'add',
     });
-    const cutterForm = async (data: any) => {
+    const cutterForm = async (data?: any) => {
       if (form.value) { (form.value as ElForm).clearValidate(); }
       if (data) {
         data = deepClone(data);
-
+        addModal.value.type = 'update';
       } else {
         data = initCutterForm();
+        addModal.value.type = 'add';
       }
       addModal.value.cutterInfo = data;
       addModal.value.visible = true;
@@ -154,12 +150,20 @@ export default {
       storeHistoryModal.value.visible = true;
     };
     async function cutterInfoUpdate() {
-      const valid = true;
+      const valid = await (form.value as ElForm).validate();
+      console.log(valid);
+      console.log(addModal.value.cutterInfo);
       if (valid) {
-        const { name, no, fitDeviceType } = addModal.value.cutterInfo;
+        if (addModal.value.type === 'add') {
+          await ComponentStoreAdd({
+            name: addModal.value.cutterInfo.name,
+            no: addModal.value.cutterInfo.no,
+            fitDeviceType: JSON.stringify(addModal.value.cutterInfo.fitDeviceType),
+          })
+        }
         addModal.value.visible = false;
         Message.success('添加成功');
-        await query();
+        cutterList.value =await ComponentStoreList();
       }
     }
     async function storeRecordUpdate() {
@@ -190,26 +194,14 @@ export default {
     };
     onMounted(useLoading(loading, async () => {
       await query();
-      cutterNameList.value = [
-        {id: '0', type: '', name: '刀具1', no: 'YC2838363738931', fitDeviceType: ['samrt200', 'smart300'], quantity: '100', createDt: '', extend: {discardNum: '20'}},
-        {id: '1', type: '', name: '刀具2', no: 'YC2838363738932', fitDeviceType: ['samrt200'], quantity: '120', createDt: '', extend: {discardNum: '11'}},
-        {id: '2', type: '', name: '刀具3', no: 'YC2838363738933', fitDeviceType: ['samrt200'], quantity: '60', createDt: '', extend: {discardNum: '15'}},
-        {id: '3', type: '', name: '刀具4', no: 'YC2838363738934', fitDeviceType: ['samrt200', 'smart300'], quantity: '55', createDt: '', extend: {discardNum: '20'}},
-        {id: '4', type: '', name: '刀具5', no: 'YC2838363738935', fitDeviceType: ['samrt200'], quantity: '32', createDt: '', extend: {discardNum: '2'}},
-      ];
-      cutterNoList.value = [
-        {id: '0', type: '', name: '刀具1', no: 'YC2838363738931', fitDeviceType: ['samrt200', 'smart300'], quantity: '100', createDt: '', extend: {discardNum: '20'}},
-        {id: '1', type: '', name: '刀具2', no: 'YC2838363738932', fitDeviceType: ['samrt200'], quantity: '120', createDt: '', extend: {discardNum: '11'}},
-        {id: '2', type: '', name: '刀具3', no: 'YC2838363738933', fitDeviceType: ['samrt200'], quantity: '60', createDt: '', extend: {discardNum: '15'}},
-        {id: '3', type: '', name: '刀具4', no: 'YC2838363738934', fitDeviceType: ['samrt200', 'smart300'], quantity: '55', createDt: '', extend: {discardNum: '20'}},
-        {id: '4', type: '', name: '刀具5', no: 'YC2838363738935', fitDeviceType: ['samrt200'], quantity: '32', createDt: '', extend: {discardNum: '2'}},
-      ];
+       cutterList.value =await ComponentStoreList();
+       deviceTypeList.value = await DeviceTypeList();
     }));
     return{
-      loading, cutterNameList, cutterName, cutterNoList, cutterNo,
+      loading, cutterNameList, keywords, cutterList, form,
       query, deviceComponentStore,
       remove: useConfirm('确认删除？', useLoading(loading, remove)),
-      addModal, cutterForm,
+      addModal, cutterForm, deviceTypeList,
       cutterInfoUpdate: useLoading(loading, cutterInfoUpdate),
       storeRecordModal, storeRecordForm,
       storeRecordUpdate: useLoading(loading, storeRecordUpdate),
@@ -220,7 +212,7 @@ export default {
 };
 function initCutterForm() {
   return {
-    name: '', no: '', fitDeviceType: '',
+    name: '', no: '', fitDeviceType: {},
   };
 }
 function initStoreRecordForm() {
