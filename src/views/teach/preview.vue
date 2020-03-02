1fd96df5ccf1434619fad1d25d3cf3da4beffa0b
jzw <1159110867@qq.com>
2020-03-02 20:55:31 +0800

    课前预习

--- src/views/teach/preview.vue
+++ src/views/teach/preview.vue
@@ -14,53 +14,60 @@
         <el-button type="primary" @click="queryCourseInfo()">预习</el-button>
       </el-form-item>
     </el-form>
-    <div style="margin: 10px 0" class="block_background" v-show="coursePreviewList.length > 0">
+    <div style="margin: 10px 0" class="block_background" v-show="coursePreviewList.length > 0" v-if="courseInfo">
       <div class="block_title flex start">课程详细信息</div>
       <div style="margin-top:20px">
-          <div style="font-weight:bold;margin: 10px 20px">自动化原理与操作</div>
-          <el-form label-width="150px" class="flex space-between wrap" style="width:60%">
-            <el-form-item label="上课时间：">{{'2020年2月10日  周一  14：30~15：30  2课时'}}</el-form-item>
-            <el-form-item label="授课教师：">{{'马冬梅'}}</el-form-item>
-            <el-form-item label="上课班级：">{{'自动化1602'}}</el-form-item>
-            <el-form-item label="上课地点：">{{'实验室'}}</el-form-item>
-            <el-form-item label="操作台：">{{'操作台02'}}</el-form-item>
+          <div style="font-weight:bold;margin: 10px 20px">{{courseInfo.course.name}}</div>
+          <el-form label-width="120px" class="flex between wrap" style="width:80%">
+            <el-form-item label="上课时间：">{{courseInfo.startDt + '-' + courseInfo.endDt}}</el-form-item>
+            <el-form-item label="授课教师：">{{courseInfo.teacher.name}}</el-form-item>
+            <el-form-item label="上课班级：">{{courseInfo.clasz.name + '-' + courseInfo.claszGroup.name}}</el-form-item>
+            <!-- <el-form-item label="上课地点：">{{'实验室'}}</el-form-item> -->
           </el-form>
-          <div style="width:95%;height:1px;margin-left:30px;padding:0px;background-color:#D5D5D5;overflow:hidden;"></div>
+          <div class="liner"></div>
       </div>
       <div style="margin-top:20px">
-        <div style="font-weight:bold;margin: 10px 20px">实验名称</div>
-        <div style="margin:20px">自动化操作</div>
-        <div style="width:95%;height:1px;margin-left:30px;padding:0px;background-color:#D5D5D5;overflow:hidden;"></div>
+        <div style="font-weight:bold;margin: 10px 20px">{{courseInfo.program.name}}</div>
+        <el-form label-width="120px" class="flex">
+          <el-form-item label="实验类型：">{{courseInfo.program.label}}</el-form-item>
+          <el-form-item label="操作台：">
+              <div v-for="item of courseInfo.stations" :key="item.id">{{item.name + ','}}</div>
+            </el-form-item>
+        </el-form>
+        <div class="liner"></div>
       </div>
       <div style="margin-top:20px">
         <div style="font-weight:bold;margin: 10px 20px">实验目的</div>
-        <div style="margin:20px">通过编程实现vigenere加密，更加深入的了解其原理，掌握vigenere算法。</div>
-        <div style="width:95%;height:1px;margin-left:30px;padding:0px;background-color:#D5D5D5;overflow:hidden;"></div>
+        <div style="margin:20px 40px">{{courseInfo.program.purpose}}</div>
+        <div class="liner"></div>
+      </div>
+      <div style="margin-top:20px">
+        <div style="font-weight:bold;margin: 10px 20px">实验原理</div>
+        <div style="margin:20px 40px">{{courseInfo.program.principle}}</div>
+        <div class="liner"></div>
       </div>
       <div style="margin-top:20px">
         <div style="font-weight:bold;margin: 10px 20px">实验步骤</div>
-        <el-form style="margin:20px">
-          <el-form-item label="1、预习内容">
-            <div>vigenere cipher加密规则：1）找到一个关键字作为密钥；2）将关键字重复的写在明文上；3）根据vigenere table 找出对应的密文</div>
-          </el-form-item>
-          <el-form-item label="2、解密">
-            <div>1）根据vigenere table找到中对应的字母；2）再看密文字母对应的关键字字母；3）唯一的确定明文字母。</div>  
-          </el-form-item>
-          <el-form-item label="3、关键代码（如何让3个字母实现类似vigenere table的形式）">
-            <div>PS：具体代码见附件</div>
-          </el-form-item>
-        </el-form>
-        <div style="width:95%;height:1px;margin-left:30px;padding:0px;background-color:#D5D5D5;overflow:hidden;"></div>
+        <div style="margin: 20px 40px">{{courseInfo.program.steps}}</div>
+        <div class="liner"></div>
+      </div>
+      <div style="margin-top:20px">
+        <div style="font-weight:bold;margin: 10px 20px">实验结果</div>
+        <div style="margin:20px 40px">{{courseInfo.program.results}}</div>
+        <div class="liner"></div>
       </div>
       <div style="margin-top:20px">
         <div style="font-weight:bold;margin: 10px 20px">课程附件</div>
-        <el-button type="text" style="margin:20px" @click="download()">关键代码.nv</el-button>
-        <div style="width:95%;height:1px;margin-left:30px;padding:0px;background-color:#D5D5D5;overflow:hidden;"></div>
+        <el-link 
+          type="primary" style="margin:20px 40px"
+          v-for="(item,i) in courseInfo.attachment" :key="i" @click="downFile(item)">{{item.split('/')[item.split('/').length-1] + '，'}}
+          </el-link>
+        <div class="liner"></div>
       </div>
       <div style="margin-top:20px">
         <div style="font-weight:bold;margin: 10px 20px">项目附件</div>
-        <el-button type="text" style="margin:20px" @click="download()">关键代码.nv</el-button>
-        <div style="width:95%;height:1px;margin-left:30px;padding:0px;background-color:#D5D5D5;overflow:hidden;"></div>
+        <el-link type="primary" style="margin:20px 40px" @click="downFile()">关键代码.nv</el-link>
+        <div class="liner"></div>
       </div>
     </div>
   </div>
@@ -71,6 +78,7 @@ import { useLoading, useConfirm } from 'web-toolkit/src/service';
 import { Message } from 'element-ui';
 import { CourseList4Student} from '@/dao/courseProgramDao';
 import { CourseRecordPreview} from '@/dao/courseRecordDao';
+import {DownLoadPrivate} from '@/dao/commonDao';
 export default {
   setup() {
     const loading = ref(false);
@@ -88,9 +96,13 @@ export default {
       });
       console.log(courseInfo.value);
     };
-    const download = async () => {
-      Message.success('下载成功');
-    };
+    async function downFile(item: any) {
+      const files = {
+        path: item,
+        filename: item.split('/')[item.split('/').length - 1],
+      };
+      await DownLoadPrivate(files.path, files.filename);
+    }
     onMounted(useLoading(loading, async () => {
       await queryCoursePreviewList();
       // await queryCourseInfo();
@@ -98,7 +110,7 @@ export default {
     return {
       loading, coursePreviewList, queryCoursePreviewList, coursePreview,
       queryCourseInfo, courseInfo,
-      download: useConfirm('确认下载？', useLoading(loading, download)),
+      downFile,
     };
   },
 };
@@ -120,5 +132,13 @@ export default {
 .i{
   font-size: 1.4rem!important;
   color: blue;
+}
+.liner{
+  width: 95%;
+  height: 1px;
+  margin-left: 30px;
+  padding: 0px;
+  background-color: #D5D5D5;
+  overflow: hidden;
 }
 </style>
