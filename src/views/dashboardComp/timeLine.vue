46606ef1d31ec231742695a6881fbf5f14027104
“FORZENLENO <1113650154@qq.com>
2020-04-11 11:24:08 +0800

    debug

--- src/views/dashboardComp/timeLine.vue
+++ src/views/dashboardComp/timeLine.vue
@@ -4,8 +4,8 @@
       <div style="background-color:#28D0F1;width:0.5rem;height:1.6rem"></div>
       <div style="color:#28D0F1;font-weight:bold;font-size:1.3rem;margin-left: 1rem">设备运行时间轴</div>
     </div>
-    <div class="flex center wrap column content" style="margin-top:-1vh;overflow:hidden;height:27vh;width:30vw;margin-left:2vw;">
-      <div class="device-card flex center" style="width:18vw;margin-left:1rem;" v-for="(item,i) in stationList" :key="i">
+    <div class="flex center wrap column content" style="margin-top:-1vh;overflow:hidden;height:27vh;width:30vw;margin-left:4vh;">
+      <div class="device-card flex center" style="width:18vw;margin-left:2vh;" v-for="(item,i) in stationList" :key="i">
         <div
           class="flex align-center center" style="margin-top:1vh;">
           <div class="device-img">
@@ -21,8 +21,8 @@
         </div>
       </div>
     </div>
-    <div class="flex center wrap column content" style="margin-top:-3vh;overflow:hidden;height:25vh;width:30vw;margin-left:2vw;">
-      <div class="device-card flex center" style="width:18vw;margin-left:1vw;" v-for="(item,i) in stationList" :key="i">
+    <div class="flex center wrap column content" style="margin-top:-3vh;overflow:hidden;height:25vh;width:30vw;margin-left:4vh;">
+      <div class="device-card flex center" style="width:18vw;margin-left:2vh;" v-for="(item,i) in stationList" :key="i">
         <div
           class="flex align-center center" style="margin-top:1vh;">
           <div class="device-img">
